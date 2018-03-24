7ac818120bfb7fe6b46f34b915716756590459e3
Patrick B <patricknbruce@gmail.com>
2018-03-24 00:23:48 -0500

    Initial Commit

---
+++ Political Party 0.1Tests/Political_Party_0_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Political_Party_0_1Tests.swift
+//  Political Party 0.1Tests
+//
+//  Created by Patrick Bruce on 3/24/18.
+//  Copyright © 2018 Patrick Bruce. All rights reserved.
+//
+
+import XCTest
+@testable import Political_Party_0_1
+
+class Political_Party_0_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
